625e381e6207102ec1fe4b4e8d597b620a5d764d
Michael Kiefer <mkiefer@exele.com>
2017-12-03 17:30:12 -0500

    initial commit

---
+++ beercember2017/Routes.vb
@@ -0,0 +1,12 @@
+﻿Imports Nancy
+
+Public Class Routes : Inherits NancyModule
+
+    Public Sub New()
+
+        [Get]("/hello") = Function() "Hello"
+        [Get]("/") = Function() View("main.html")
+
+    End Sub
+
+End Class
