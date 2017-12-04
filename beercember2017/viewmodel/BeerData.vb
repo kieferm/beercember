d7f7abf3439be5571e34833c24bce1a460f191ce
Michael Kiefer <mkiefer@exele.com>
2017-12-03 19:47:57 -0500

    add debug dates

--- beercember2017/viewmodel/BeerData.vb
+++ beercember2017/viewmodel/BeerData.vb
@@ -1,3 +1,9 @@
 ﻿Public Class BeerData
     Public Property Beers As New List(Of BeerDay)
+
+    Public Property RefDate As Date
+    Public Property CurTime As Date
+    Public Property CurDate As Date
+
+
 End Class
