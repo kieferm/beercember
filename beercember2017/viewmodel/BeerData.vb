d26f3211e165e6adea769856a143e6f07eb0ff24
Michael Kiefer <mkiefer@exele.com>
2017-12-04 19:36:25 -0500

    change datat timestamp

--- beercember2017/viewmodel/BeerData.vb
+++ beercember2017/viewmodel/BeerData.vb
@@ -1,7 +1,9 @@
 ﻿Public Class BeerData
     Public Property Beers As New List(Of BeerDay)
-    Public Property DataTimestamp As Date
+    Public Property DataTimestamp As String
+
     Public Sub New()
-        DataTimestamp = Date.Now()
+        DataTimestamp = Date.Now().AddHours(-5).ToString()
     End Sub
+
 End Class
